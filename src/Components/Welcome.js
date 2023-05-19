6c77dd908ee3c72648bddbd82b5e32188b1ac4f7
Manojdeore7 <manojddeore11>
2023-05-19 15:26:12 +0530

    expense are added

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useRef, useState } from "react";
 import AuthContext from "../Store/AuthContext";
 function Welcome() {
   let context = useContext(AuthContext);
   let token = context.Token;
+  let amountRef = useRef();
+  let desRef = useRef();
+  let catRef = useRef();
+  let [array, setArray] = useState([]);
   function clickHandler() {
     fetch(
       "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyAgl36Y2mjDOhSlZShpe33Xk4fWzEhi6TE",
@@ -53,6 +57,17 @@ function Welcome() {
       });
     }, 5000);
   }
+  function AddExpense(e) {
+    e.preventDefault();
+    let obj = {
+      Amount: amountRef.current.value,
+      Description: desRef.current.value,
+      Category: catRef.current,
+    };
+    setArray((prev) => {
+      prev.push(obj);
+    });
+  }
   return (
     <div className="row ">
       <div className="col-4 ">
@@ -60,9 +75,43 @@ function Welcome() {
       </div>
       <div className="col-4 offset-4">
         <h4>
-          your profile is incomplete{" "}
+          your profile is incomplete
           <Link to="./Welcome/profile">Complete Now!</Link>
         </h4>
+      </div>
+      <div className="col-8">
+        <title>Add Expenses</title>
+        <form onSubmit={AddExpense}>
+          <label>Amount</label>
+          <input type="number" ref={amountRef}></input>
+          <label>Description </label>
+          <input type="text" ref={desRef}></input>
+          <label>category</label>
+          <input type="text" ref={catRef}></input>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+      <div className="col-8">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Amount</th>
+              <th>Description</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {array.map((obj) => {
+              return (
+                <tr>
+                  <td>{obj.Amount}</td>
+                  <td>{obj.Description}</td>
+                  <td>{obj.Category}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
       <div>
         <button className="btn btn-primary" onClick={clickHandler}>
